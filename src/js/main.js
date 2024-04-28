a70c57d2161cf905a1d0b6950d8db5456e12adc2
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2024-04-28 23:19:20 +0100

    feat: fix validation page

--- src/js/main.js
+++ src/js/main.js
@@ -3,37 +3,74 @@ import auth from './auth';
 const authenticationPage = document.querySelector('#authentication-page');
 const signInButton = document.querySelector('#sign-in-button');
 const signOutButton = document.querySelector('#sign-out-button');
-const formButton = document.querySelector('#form-button');
 const todosPage = document.querySelector('#todos-page');
+const formButton = document.querySelector('#form');
 
 const authHandler = auth();
 
-const signIn = () => {
+const signIn = (email) => {
   authenticationPage.classList.replace('flex', 'hidden');
   todosPage.classList.replace('hidden', 'flex');
+  authHandler.signIn(email);
 };
 
 const signOut = () => {
   authenticationPage.classList.replace('hidden', 'flex');
   todosPage.classList.replace('flex', 'hidden');
+  authHandler.signOut();
 };
 
-const submitEvent = (event) => {
+formButton.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  signInButton.addEventListener('click', signIn, () => {
-    const emailInput = document.querySelector('#email');
-
-    const email = emailInput.value;
-    try {
-      authHandler.signIn(email);
-    } catch (error) {
-      error.message = error;
-    }
-  });
-};
-
-formButton.addEventListener('submit', submitEvent);
-signOutButton.addEventListener('click', signOut, () => {
-  authHandler.signOut();
+  const emailInput = document.querySelector('#email');
+  const email = emailInput.value;
+  try {
+    authHandler.signIn(email);
+  } catch (error) {
+    error.message = error;
+  }
 });
+
+signInButton.addEventListener('click', signIn);
+signOutButton.addEventListener('click', signOut);
+
+// import auth from './auth';
+
+// const authenticationPage = document.querySelector('#authentication-page');
+// const signInButton = document.querySelector('#sign-in-button');
+// const signOutButton = document.querySelector('#sign-out-button');
+// const formButton = document.querySelector('#form-button');
+// const todosPage = document.querySelector('#todos-page');
+
+// const authHandler = auth();
+
+// const signIn = () => {
+//   authenticationPage.classList.replace('flex', 'hidden');
+//   todosPage.classList.replace('hidden', 'flex');
+// };
+
+// const signOut = () => {
+//   authenticationPage.classList.replace('hidden', 'flex');
+//   todosPage.classList.replace('flex', 'hidden');
+// };
+
+// const submitEvent = (event) => {
+//   event.preventDefault();
+
+//   signInButton.addEventListener('click', signIn, () => {
+//     const emailInput = document.querySelector('#email');
+
+//     const email = emailInput.value;
+//     try {
+//       authHandler.signIn(email);
+//     } catch (error) {
+//       error.message = error;
+//     }
+//   });
+// };
+
+// formButton.addEventListener('submit', submitEvent);
+// signOutButton.addEventListener('click', signOut, () => {
+//   authHandler.signOut();
+// });
